8568a111d734e6550984f836dcd83803bd6d5dd8
Austin Wang <yyaustin@bu.edu>
2023-03-23 20:01:37 -0400

    Finish exercise 5.15

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -6,17 +6,24 @@ import Blog from "./Blog"
 
 // ({ blog, addLike, removeBlog })
 describe("<Blog />", () => {
-  test("<Blog /> only shows title and author by default", () => {
-    const blog = {
+  let blog
+  let addLike
+  let removeBlog
+
+  beforeEach(() => {
+    blog = {
       title: "test blog",
       author: "react-testing-library",
       url: "http://localhost:3001",
       likes: 10,
       id: "test"
     }
-
-    render(<Blog blog={blog} />)
-
+    addLike = jest.fn()
+    removeBlog = jest.fn()
+    render(<Blog blog={blog} addLike={addLike} removeBlog={removeBlog} />)
+  })
+  
+  test("<Blog /> only shows title and author by default", () => {
     const titleElement = screen.getByText(blog.title)
     const authorElement = screen.queryByText(blog.author)
     const urlElement = screen.queryByText(blog.url)
@@ -26,5 +33,31 @@ describe("<Blog />", () => {
     expect(authorElement).not.toBeInTheDocument()
     expect(urlElement).not.toBeInTheDocument()
     expect(likesElement).not.toBeInTheDocument()
+  })
+
+  test("<Blog /> shows all info after clicking the view button", async () => {
+    const viewButton = screen.getByRole("button", { name: "view" })
+    await userEvent.click(viewButton)
+
+    const titleElement = screen.getByText(blog.title, { exact: false })
+    const authorElement = screen.getByText(blog.author, { exact: false })
+    const urlElement = screen.getByText(blog.url, { exact: false })
+    const likesElement = screen.getByText(blog.likes, { exact: false })
+
+    expect(titleElement).toBeInTheDocument()
+    expect(authorElement).toBeInTheDocument()
+    expect(urlElement).toBeInTheDocument()
+    expect(likesElement).toBeInTheDocument()
+  })
+
+  test("<Blog /> calls addLike twice when like button is clicked twice", async () => {
+    const viewButton = screen.getByRole("button", { name: "view" })
+    await userEvent.click(viewButton)
+
+    const likeButton = screen.getByRole("button", { name: "like" })
+    await userEvent.click(likeButton)
+    await userEvent.click(likeButton)
+
+    expect(addLike).toHaveBeenCalledTimes(2)
   })
 })
